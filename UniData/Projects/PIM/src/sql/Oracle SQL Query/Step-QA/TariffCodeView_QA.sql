1f8d8c5857685b567ac03bcf8ff17e2adbe6551d
Junjie <junjie.tang@hotmail.com>
2017-11-06 14:34:54 -0800

    minor update

--- UniData/Projects/PIM/src/sql/Oracle SQL Query/Step-QA/TariffCodeView_QA.sql
+++
@@ -1,9 +0,0 @@
-exec pimviewapipck.setviewcontext('Context1','Approved');
-
-select name TariffCodeID, pimviewapipck.getcontextname(id) TariffCode, 
-  pimviewapipck.getcontextvalue4node(id,1762250) TariffCodeCA,
-  pimviewapipck.getcontextvalue4node(id,1762242) TariffCodeUS,
-  pimviewapipck.getcontextvalue4node(id,1762028) TariffCodeCode,
-  pimviewapipck.getcontextvalue4node(id,1762198) TariffCodeLongDescription,
-  pimviewapipck.getcontextvalue4node(id,1762222) TariffCodeShortDescription
-from entity_all where subtypeid=1754570
